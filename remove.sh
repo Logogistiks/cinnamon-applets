487bab92ab1deff27c53b2520fcdf42773990777
Jan <92688175+Logogistiks@users.noreply.github.com>
2026-09-12 00:31:43 +0200

    qwert

--- remove.sh
+++ remove.sh
@@ -6,5 +6,6 @@ APPLET_DIR="$HOME/.local/share/cinnamon/applets"
 for dir in "$SCRIPT_DIR"/*@logogistiks; do
     if [[ -d "$dir" && -f "$dir/metadata.json" ]]; then
         rm -f "$APPLET_DIR/$(basename "$dir")"
+        rm -f "$dir/shared"
     fi
 done
